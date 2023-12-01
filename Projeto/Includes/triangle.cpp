9ef74fa12127d8555754d5269ddf26602cc22f1e
Thiago de Souza Farias <tsf@cin.ufpe.br>
2023-12-01 18:36:06 -0300

    simplifica o calculo dos coeficientes

--- Projeto/Includes/triangle.cpp
+++ Projeto/Includes/triangle.cpp
@@ -2,42 +2,25 @@
 
 bool triangle::hit(const ray &r, float t_min, float t_max, hit_record &rec) const
 {
-    float t = glm::dot((v1 - r.location()), normal) / glm::dot(r.direction(), normal);
+    float t = glm::dot((a - r.location()), normal) / glm::dot(r.direction(), normal);
 
     if (t < t_max && t > t_min)
-    { // Teve interseção com o plano do triângulo
-        // Ponto de interseção:
-        glm::vec3 pontoIntersecao = r.point_at_parameter(t);
+    {
+        // Ponto de interseção com o plano:
+        glm::vec3 p = r.point_at_parameter(t);
+        
+        // Vetor do vertice do triangulo até o ponto p
+        glm::vec3 w = p - a;
+        
+        float beta = glm::dot(w, hb);
+        float gamma = glm::dot(w, hc);
+        float alpha = 1 - beta - gamma;
 
-        const float EPSILON = 0.000001f;
-        glm::vec3 edge1 = v1 - v0;
-        glm::vec3 edge2 = v2 - v0;
-        glm::vec3 h = glm::cross(r.direction(), edge2);
-        float a = glm::dot(edge1, h);
-
-        if (a > -EPSILON && a < EPSILON)
-            return false; // O raio é paralelo ao triângulo
-
-        float f = 1.0f / a;
-        glm::vec3 s = r.location() - v0;
-        float u = f * glm::dot(s, h);
-
-        if (u < 0.0f || u > 1.0f)
-            return false;
-
-        glm::vec3 q = glm::cross(s, edge1);
-        float v = f * glm::dot(r.direction(), q);
-
-        if (v < 0.0f || u + v > 1.0f)
-            return false;
-
-        float t = f * glm::dot(edge2, q);
-
-        if (t > t_min && t < t_max)
+        if (alpha >= 0 && beta >= 0 && gamma >= 0)
         {
             rec.t = t;
-            rec.p = r.point_at_parameter(t);
-            rec.normal = glm::normalize(glm::cross(edge1, edge2));
+            rec.p = p;
+            rec.normal = normal;
             rec.cor = cor;
             return true;
         }
--- Projeto/Includes/triangle.h
+++ Projeto/Includes/triangle.h
@@ -6,13 +6,42 @@
 class triangle: public hitable {
     public:
         triangle() {}
-        triangle(glm::vec3 v0, glm::vec3 v1, glm::vec3 v2,  glm::vec3 n, glm::vec3 c):v0(v0), v1(v1), v2(v2), normal(n), cor(c) {};
+        triangle(glm::vec3 a, glm::vec3 b, glm::vec3 c,  glm::vec3 n, glm::vec3 color):a(a), b(b), c(c), normal(n), cor(color) {
+            // vetor da aresta entre a e b
+            glm::vec3 u = b - a;
+
+            //vetor da aresta entre a e c
+            glm::vec3 v = c - a;
+
+            //projecao do vetor u em v
+            glm::vec3 projuv = dot(u,v)/dot(v,v) * v;
+
+            //projecao do vetor v em u
+            glm::vec3 projvu = dot(v,u)/dot(u,u) * u;
+
+            // altura do ponto B
+            hb = u - projuv;
+
+            // normalização inversa da altura hb
+            hb = hb/dot(hb,hb);
+
+            // altura do ponto C
+            hc = v - projvu;
+
+            // normalização inversa da altura hc
+            hc = hc/dot(hc,hc);
+
+        };
         virtual bool hit(const ray& r, float t_min, float t_max, hit_record& rec) const;
-        glm::vec3 v0;
-        glm::vec3 v1;
-        glm::vec3 v2;
+        glm::vec3 a;
+        glm::vec3 b;
+        glm::vec3 c;
         glm::vec3 normal;
         color cor;
+
+        glm::vec3 hb;
+        glm::vec3 hc;
+
 };
 
 
