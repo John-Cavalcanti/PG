0b2953e9c61a3e08b0f1e29d1942c99bb045b23e
Thiago de Souza Farias <tsf@cin.ufpe.br>
2023-12-02 14:09:05 -0300

    move construtor do triângulo para o .cpp

--- Projeto/Includes/triangle.cpp
+++ Projeto/Includes/triangle.cpp
@@ -1,5 +1,32 @@
 #include "./triangle.h"
 
+// construtor do triangle
+triangle::triangle(glm::vec3 a, glm::vec3 b, glm::vec3 c,  glm::vec3 n, glm::vec3 color):a(a), b(b), c(c), normal(n), cor(color) {
+            // vetor da aresta entre a e b
+            glm::vec3 u = b - a;
+
+            // vetor da aresta entre a e c
+            glm::vec3 v = c - a;
+
+            // projecao do vetor u em v
+            glm::vec3 projuv = dot(u,v)/dot(v,v) * v;
+
+            // projecao do vetor v em u
+            glm::vec3 projvu = dot(v,u)/dot(u,u) * u;
+
+            // altura do ponto B
+            hb = u - projuv;
+
+            // normalização inversa da altura hb
+            hb = hb/dot(hb,hb);
+
+            // altura do ponto C
+            hc = v - projvu;
+
+            // normalização inversa da altura hc
+            hc = hc/dot(hc,hc);
+        };
+
 bool triangle::hit(const ray &r, float t_min, float t_max, hit_record &rec) const
 {
     float t = glm::dot((a - r.location()), normal) / glm::dot(r.direction(), normal);
--- Projeto/Includes/triangle.h
+++ Projeto/Includes/triangle.h
@@ -6,32 +6,7 @@
 class triangle: public hitable {
     public:
         triangle() {}
-        triangle(glm::vec3 a, glm::vec3 b, glm::vec3 c,  glm::vec3 n, glm::vec3 color):a(a), b(b), c(c), normal(n), cor(color) {
-            // vetor da aresta entre a e b
-            glm::vec3 u = b - a;
-
-            //vetor da aresta entre a e c
-            glm::vec3 v = c - a;
-
-            //projecao do vetor u em v
-            glm::vec3 projuv = dot(u,v)/dot(v,v) * v;
-
-            //projecao do vetor v em u
-            glm::vec3 projvu = dot(v,u)/dot(u,u) * u;
-
-            // altura do ponto B
-            hb = u - projuv;
-
-            // normalização inversa da altura hb
-            hb = hb/dot(hb,hb);
-
-            // altura do ponto C
-            hc = v - projvu;
-
-            // normalização inversa da altura hc
-            hc = hc/dot(hc,hc);
-
-        };
+        triangle(glm::vec3 a, glm::vec3 b, glm::vec3 c,  glm::vec3 n, glm::vec3 color);
         virtual bool hit(const ray& r, float t_min, float t_max, hit_record& rec) const;
         glm::vec3 a;
         glm::vec3 b;
@@ -39,7 +14,9 @@ class triangle: public hitable {
         glm::vec3 normal;
         color cor;
 
+        // Inverso da altura do ponto B
         glm::vec3 hb;
+        // Inverso da altura do ponto C
         glm::vec3 hc;
 
 };
