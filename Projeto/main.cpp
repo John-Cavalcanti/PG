a6e1bf7ca0feae3a5666bd4d5ac673bc3074667f
Thiago de Souza Farias <tsf@cin.ufpe.br>
2023-12-07 10:19:46 -0300

    renderiza um icosaedro na cena

--- Projeto/main.cpp
+++ Projeto/main.cpp
@@ -65,23 +65,49 @@ int main() {
     list[2] = new sphere(glm::vec3(0, -2.0, -5.0), 1, blue);
 
     // numeros de triangulos
-    int t = 2;
+    int t = 20;
 
     // numero total de vertices
-    int v = 4;
+    int v = 12;
     
     // Uma lista de vértices (pontos da mesh) 
     vec3 pontos[v] = {
-        vec3(-6,-4,-5),
-        vec3(-3,-1,-5),
-        vec3(-6,0,-5),
-        vec3(-17,-2,-11)
+        vec3(-2.0,-0.9742688878808665,-1.1493491916479601),
+        vec3(-2.0,-2.0257311121191335,-1.1493491916479601),
+        vec3(-1.1493491916479597,-1.5,-1.4742688878808665),
+        vec3(-1.474268887880866,-0.6493491916479601,-2.0),
+        vec3(-2.525731112119134,-0.6493491916479601,-2.0),
+        vec3(-2.8506508083520403,-1.5,-1.4742688878808665),
+        vec3(-2.0,-0.9742688878808665,-2.85065080835204),
+        vec3(-2.0,-2.0257311121191335,-2.85065080835204),
+        vec3(-2.8506508083520403,-1.5,-2.5257311121191335),
+        vec3(-1.1493491916479597,-1.5,-2.5257311121191335),
+        vec3(-1.474268887880866,-2.35065080835204,-2.0),
+        vec3(-2.525731112119134,-2.35065080835204,-2.0)
     };
 
     // Uma lista com triplas de índices de vértices (cada tripla possui os índices dos vértices (na lista de vértices) que fazem parte de um triângulo)
     triple vertices_index[t] = {
-        triple(0, 2, 3),
-        triple(0, 1, 2)
+        triple(0,1,2),
+        triple(0,3,4),
+        triple(0,4,5),
+        triple(0,2,3),
+        triple(0,5,1),
+        triple(6,7,8),
+        triple(6,3,9),
+        triple(6,4,3),
+        triple(6,9,7),
+        triple(6,8,4),
+        triple(1,10,2),
+        triple(1,11,10),
+        triple(1,5,11),
+        triple(7,10,11),
+        triple(7,11,8),
+        triple(7,9,10),
+        triple(3,2,9),
+        triple(10,9,2),
+        triple(4,8,5),
+        triple(11,5,8),
     }; 
     list[3] = new tmesh(v, t, pontos, vertices_index, green+red);
     
