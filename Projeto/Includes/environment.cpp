5b07bb7c7a739e7f015eb1f47293f8198862bc7e
Tales Cunha <tvac@cin.ufpe.br>
2024-02-20 13:23:18 -0300

    Refactor Light class constructor and remove unused methods

---
+++ Projeto/Includes/environment.cpp
@@ -0,0 +1,14 @@
+#include "environment.h"
+
+// Define o construtor da classe Environment
+// O construtor recebe a luz ambiente
+// O valor é usado para inicializar o atributo da classe
+Environment::Environment(const glm::ivec3& ambientLight)
+    : ambientLight(ambientLight) {}
+
+// Define o metodo getAmbientLight da classe Environment
+// O metodo retorna a luz ambiente como um vetor de 3 dimensoes com valores inteiros
+// Os valores representam a intensidade da luz ambiente em cada canal de cor (RGB)
+glm::ivec3 Environment::getAmbientLight() const {
+    return ambientLight;
+}
--- Projeto/Includes/light.cpp
+++ Projeto/Includes/light.cpp
@@ -1,27 +1,19 @@
 #include "light.h"
 // Define o construtor da classe Light
-// O construtor recebe a posicao da luz, a intensidade e a luz ambiente
+// O construtor recebe a posicao da luz e a intensidade
 // Os valores sao usados para inicializar os atributos da classe
-Light::Light(const glm::vec3& position, const glm::ivec3& intensity, const glm::ivec3& ambientLight)
-    : position(position), intensity(intensity), ambientLight(ambientLight) {}
+Light::Light(const glm::vec3& position, const glm::ivec3& intensity)
+    : position(position), intensity(intensity) {}
 
 // Define o metodo getPosition da classe Light
 // O metodo retorna a posicao da luz como um vetor de 3 dimensoes
-
 glm::vec3 Light::getPosition() const {
     return position;
 }
+
 // Define o metodo getIntensity da classe Light 
 // O metodo retorna a intensidade da luz como um vetor de 3 dimensoes com valores inteiros
 // Os valores representam a intensidade da luz em cada canal de cor (RGB)
-
 glm::ivec3 Light::getIntensity() const {
     return intensity;
-}
-
-// Define o metodo getAmbientLight da classe Light
-// O metodo retorna a luz ambiente como um vetor de 3 dimensoes com valores inteiros
-// Os valores representam a intensidade da luz ambiente em cada canal de cor (RGB)
-glm::ivec3 Light::getAmbientLight() const {
-    return ambientLight;
 }
