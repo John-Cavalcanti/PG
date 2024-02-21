38b5e7219a35c1205e8878f867093dc708a87304
João Victor Cavalcanti <jvictorao123@gmail.com>
2024-02-21 09:37:56 -0300

    imagens de progresso salvas

--- Projeto/main.cpp
+++ Projeto/main.cpp
@@ -112,8 +112,8 @@ void readfile();
 int main() {
     readfile();
     // largura e altura da tela respectivamente // resolução
-    int nx = 500; // hres
-    int ny = 500;  // vres
+    int nx = 1280; // hres
+    int ny = 720;  // vres
 
     std::cout << "P3\n" << nx << " " << ny << "\n255\n";
 
@@ -176,7 +176,7 @@ void readfile(){
                 float x, y, z, nx, ny, nz;
                 sscanf(line.c_str(), "p %f %f %f %f %f %f %f %f %f", &x, &y, &z, &nx, &ny, &nz, &Or, &Og, &Ob);
                 color cor = glm::vec3(Or, Og, Ob);
-                lista.push_back(new plane(glm::vec3(x, y, z), glm::vec3(nx, ny, nz), cor, glossy));
+                lista.push_back(new plane(glm::vec3(x, y, z), glm::vec3(nx, ny, nz), cor, matte));
             }
             if(line[0] == 't'){
                 // Quantidade de vertices (pontos) na mesh
